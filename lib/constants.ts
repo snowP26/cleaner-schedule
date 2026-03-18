06d8c8305ede6c672471cb9f047672f9f5f0393b
Melissa <melissanagrampa2015@gmail.com>
2026-03-18 08:49:19 +0800

    fix: change of member rotations

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const ROTATION = ['James', 'Mark', 'Jayp', 'Ken', 'Mel', 'Gian', 'JC'];
+export const ROTATION = ['James', 'Jayp', 'Mel', 'JC'];
 export const WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 export const TIME_ZONE = 'Asia/Manila';
 
